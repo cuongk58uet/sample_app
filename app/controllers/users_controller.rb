d719ea1fadb92020e9d2f3e5d686a772fd7f48d8
Nguyen Manh Cuong <cuongnm4215@gmail.com>
2017-06-08 08:54:05 +0700

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,30 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
+  end
+
+  def show
+    @user = User.find_by id: params[:id]
+    return if @user
+    flash[:warning] = t ".not_found"
+    redirect_to root_url
+  end
+
+  def create
+    @user = User.new user_params
+
+    if @user.save
+      flash[:success] = t ".signup_success"
+      redirect_to @user
+    else
+      flash[:warning] = t ".signup_error"
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit :name, :email, :password, :password_confirmation
   end
 end
